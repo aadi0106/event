c598a36a9ea728a3b2dca2e4382875465f82e433
aadi0106 <aaduagrawal19@gmail.com>
2025-09-10 20:21:07 +0530

    in

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsExports = {
+  Auth: {
+    region: "ap-south-1", // your region
+    userPoolId: "ap-south-1_K7mmUg9UR",
+    userPoolWebClientId: "2busuenclk4fvj52dp5i1q6f78",
+    mandatorySignIn: true,
+  },
+};
+export default awsExports;
